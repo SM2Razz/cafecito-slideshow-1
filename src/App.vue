879f5c269c72d3e077739023ade41b8f35fb8271
Sylvia Moore <sylvia.moore@myresman.com>
2023-05-30 14:52:51 -0500

    uploading files for initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div id="app">
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+      <h1>I am the APP!</h1>
+      <div class="wrapper">
+        <HelloWorld msg="Hi Hi hello!" />
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
